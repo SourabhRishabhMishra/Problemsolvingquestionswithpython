57e57ea0c099677019a3b42dee6422ea5dcc5ee7
NITIN KUMAR <nk767909@gmail.com>
2022-12-21 22:24:22 +0530

    python program to remove multiple elements

--- rmv_multiple_elememts_from_list.py
+++ rmv_multiple_elememts_from_list.py
@@ -1,18 +1,18 @@
 def rmv_multiple_ele(l1):
 # l1=[1,2,3,4,5]  , l2=[3,4]
     l2=[]
-    z=int(input(" enter how many elements you do not  want to put in the list :- "))
+    z=int(input(" Enter thoose elements you do not want in your list :- "))
     for j in range(z):
-        print(" enter element you do not want number ",j+1,":-",end=" ")
+        print(" Enter unwanted elements ",j+1,":-",end=" ")
         m=int(input())
         l2.append(m)
     l3 = []
     l3.extend(l1) # type of copying one list to another list 
     for ele in l1:
         if ele in l2:
-            print("remove elememnt :- ",ele)
+            # print("remove elememnt :- ",ele)
             l3.remove(ele)
-            print("new value of l3 :- ",l3)
+            # print("new value of l3 :- ",l3)
     return l3  
             
 l1 = [] 
@@ -22,40 +22,5 @@ for i in range(x):
     n=int(input())
     l1.append(n) 
 
-
 out = rmv_multiple_ele(l1)
 print(out)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''l1 = [1,2,3,4,5,6] 
-print("before")
-print(l1)
-l2 = [4,5]
-for i in l1:
-    for j in l2:
-        if l1[i] in l2[j]:
-            del i
-print("after")
-print(l1) '''
